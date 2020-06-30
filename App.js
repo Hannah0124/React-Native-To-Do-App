b3ce05ff55f7779810ee8daf759cbdaa2a6dbb02
Hannah <hannahjoo24@gmail.com>
2020-06-30 16:46:59 -0700

    Clean App.js

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native'; // FlatList: infinite view
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
-    setCourseGoals([...courseGoals, enteredGoal]); // [previousState, newState]
+  const addGoalHandler = (goalTitle) => {
+    // setCourseGoals([...courseGoals, enteredGoal]); // [previousState, newState]
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { 
+        key: Math.random().toString(), 
+        value: goalTitle
+      }
+    ]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Course Goal" 
-          style={styles.input} 
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
-      <View>
-        {courseGoals.map((goal, i) => <View style={styles.listItem}><Text key={i}>{goal}</Text></View>)}
-      </View>
+      <GoalInput onAddGoal={addGoalHandler}/>
+
+      <FlatList 
+        keyExtractor={(item, index) => item.key}
+        data={courseGoals} 
+        renderItem={itemData =>
+          <GoalItem title={itemData.item.value} />
+        } 
+      />
+
+      {/* Same as above */}
+      {/* {courseGoals.map((goal, i) => <View style={styles.listItem}><Text key={i}>{goal}</Text></View>)} */}
     </View>
   );
 }
@@ -35,23 +40,6 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%', 
-    borderBottomColor: 'black', 
-    borderBottomWidth: 1
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: "#ccc",
-    borderColor: 'black',
-    borderWidth: 1
   }
 });
 
